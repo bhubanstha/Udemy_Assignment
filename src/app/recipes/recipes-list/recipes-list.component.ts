c285f0061824df1be4bf2081af8d86727f7f7f86
= <bhubanshrestha12@gmail.com>
2018-01-17 17:54:31 +0545

    Course project initial project state

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Butter Pizza', 'http://www.wasamundi.com/farm/access/var/watermark/wtmk_5462257a9dae5141571826610557039_364392123736199_2315526976401070025_o.jpg'),
+    new Recipe('Burger', 'Big burger' , 'https://commentform.marketforce.com/images/BK-WebComment/BB_WHOPPER-v1.png')
+  ];
   constructor() { }
 
   ngOnInit() {
